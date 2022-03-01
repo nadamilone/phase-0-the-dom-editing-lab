829cf710d4811cfc7ac1a604f4f68afacba36333
NADA MILONE <nadamilone@gmail.com>
2022-02-28 20:22:18 -0500

    commit tree 0f56a51fc4ff3a8954262b0cf87879dc3110dffe

---
+++ index.js
@@ -0,0 +1,6 @@
+    //1) contains an <h1> tag
+    // 2) contains a <p> tag
+    // 3) within the <p>, it contains a <strong> tag
+    // 4) within the <p>, it contains an <em> tag
+    // 5) within the <p>, it contains an <a> tag
+    // 6) within the <body>, it contains an <table> tag
